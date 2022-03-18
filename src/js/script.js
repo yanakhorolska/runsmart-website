162ce2d291137a4e454bff2ad7a10c5464e79117
yanakhorolska <yana.khorolska@protonmail.com>
2022-03-18 14:08:19 +0100

    add responsivity for slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,14 @@
 ﻿const slider = tns({
     container: '.carousel__inner',
-    items: 1,
     slideBy: 'page',
     autoplay: false,
     controls: false,
-    speed: 1000,
+    speed: 1500,
     nav: false
-  });
-  document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
-  });
-  document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
+});
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
   });
