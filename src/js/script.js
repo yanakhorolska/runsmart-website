5f1bc28fba24c70a541633b9f7deb1511c037a87
yanakhorolska <yana.khorolska@protonmail.com>
2022-03-20 23:09:00 +0100

    add modals

--- src/js/script.js
+++ src/js/script.js
@@ -37,6 +37,23 @@ document.querySelector('.next').addEventListener('click', function () {
         });
     });
 
+          // modal
 
+          $('[data-modal="consultation"]').on('click', function() {
+            $('.overlay, #consultation').fadeIn('slow');
+          });
+          $('.modal__close').on('click', function() {
+            $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+          });
+          // $('.button_mini').on('click', function() {
+          //   $('.overlay, #order').fadeIn('slow');
+          // });
+
+          $('.button_mini').each(function(i) {
+              $(this).on('click', function() {
+                $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+                $('.overlay, #order').fadeIn('slow');
+              });
+          });
     });
     })(jQuery);
